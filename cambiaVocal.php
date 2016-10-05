98566fcfa3e59980da40c235dd7e96a8c9da5e9e
jbarrachina@ausiasmarch.net <jbarrachina@ausiasmarch.net>
2016-10-05 12:48:03 +0200

    Versión inicial 1

--- cambiaVocal.php
+++ cambiaVocal.php
@@ -7,8 +7,8 @@
 include 'lib/utilidades.php';
 
 $frase = $_POST['frase'];
-
 $vocales = "aeiouplr";
 for ($i=0;$i<strlen($vocales);$i++){
     echo sustituyeLetra($frase, $vocales[$i])."<br>";
 }
+
