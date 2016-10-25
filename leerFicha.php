2d643ace23e7d85bab8a67d2518112c42c372ad5
jose <jose@jose-Lenovo-G50-70>
2016-10-25 22:37:56 +0200

    Ficha cliente simple con jquery.validate

---
+++ leerFicha.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo 'Gracias '.$_POST['nombre'].' '.$_POST['email'].'. La información ha sido enviada correctamente!';
+?>
